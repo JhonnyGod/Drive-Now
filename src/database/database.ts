0d6f9659954f5c48b1bb181cb759f86192703d6e
Jhonny <jhony.duque@correounivalle.edu.co>
2024-08-29 22:31:35 -0500

    Initial Repository

---
+++ src/database/database.ts
@@ -0,0 +1,1 @@
+//* Conexión a la base de datos e inicialización del AppDataSource
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,5 @@
+//* Ejecución y configuración de express;
 
+import "reflect-metadata";
+
+console.log("jairo es gay");
